5fbf243cb9b301b3b2116d9641d5294bf7004358
zakhar.malinovskiy <zakhar.malinovskiy@gmail.com>
2018-08-21 10:15:34 +0300

    added sp_checkBackup

---
+++ choose_old_backups.sql
@@ -0,0 +1,75 @@
+﻿create procedure choose_old_backups as begin
+declare @t table (subdirectory nvarchar(200),depth int, isfile int);
+--получить список файлов
+insert into @t exec master.sys.xp_dirtree N'h:\za\',1,1;
+if not object_id('tempdb..#prom1') is null drop table #prom1;
+--подобрать к файлу имя базы, возможно не одно
+select  distinct files.subdirectory, bases.name, bases.lenname 
+	into #prom1
+	from @t as files
+	left JOIN (select name, len(name) as lenname from master.sys.databases) as lenOfnames
+	on 1=1
+	left JOIN (select name, len(name) as lenname from master.sys.databases) as bases
+	on substring(files.subdirectory,1, lenOfnames.lenname) = bases.name
+where isfile = 1
+and  not bases.name is null;
+
+if not OBJECT_id('tempdb..#files_') is null drop table #files_;
+
+--если к файлу подходит две базы по имени ты выбрать ту название которой длиннее
+select p1.subdirectory, p1.name, p1.lenname, p2.maxlen, 
+	convert(datetime, 
+		substring(p1.subdirectory,p1.lenname+2,10)+'T'+replace(SUBSTRING(p1.subdirectory,p1.lenname+13,8),'-',':')
+		, 127) as date1
+	into #files_
+	from #prom1 as p1 
+	inner join (
+		select subdirectory, max(lenname) as maxlen 
+		from #prom1 
+		group by subdirectory) as p2 
+	on p1.subdirectory = p2.subdirectory and  p1.lenname = p2.maxlen 
+
+--выдает те файлы у которых есть хотябы 1 full
+--до вызова процедуры надо сделать create table #files(subdirectory nvarchar(200), name nvarchar(100), date1 datetime, maxdate datetime);
+insert #files select fa.subdirectory, fa.name, fa.date1, maxi.maxdate 
+from #files_ as fa
+inner join (
+	select f.name, max(f.date1) as maxdate
+	from #files_ as f
+	where f.subdirectory like '%FULL%'
+	group by f.name) as maxi
+	on maxi.name = fa.name and fa.date1 < dateadd(dd,-2,maxi.maxdate)
+---выдает те файлы у которых нет full
+union all
+select fa.subdirectory, fa.name, fa.date1, maxi.maxdate 
+from #files_ as fa
+inner join (
+	select f.name, max(f.date1) as maxdate
+	from #files_ as f
+	group by f.name) as maxi
+	on maxi.name = fa.name and fa.date1 < dateadd(dd,-2,maxi.maxdate)
+where not (fa.name) in (select distinct f1.name from #files_ as f1 where f1.subdirectory like '%FULL%')
+end
+go
+create procedure delete_choosed_old_backups as begin
+if not OBJECT_id('tempdb..#files') is null drop table #files;
+create table #files(subdirectory nvarchar(200), name nvarchar(100), date1 datetime, maxdate datetime);
+exec master..choose_old_backups;
+declare c cursor for select subdirectory from #files
+	--where subdirectory like '%Armat%'; --debug
+open c
+declare @curfile nvarchar(200) = '';
+declare @sql nvarchar(max) = '';
+fetch next from c into @curfile
+while @@FETCH_STATUS = 0 begin
+	
+	set @sql = 'exec master.sys.xp_delete_file 0,N''h:\za\'+@curfile+'''';
+	--print @sql
+	exec sp_executesql @sql;
+	fetch next from c into @curfile
+
+end
+close c
+deallocate c
+end
+go
---
+++ run sp_checkBackup_for2000.sql
@@ -0,0 +1,240 @@
+﻿--restore database tempoDb from disk = 'j:\b\ubcd_sklad_2010_2018-04-04T22-18-16-530-FULL' with  MOVE 'UBCD_sklad_2010_Data' TO 'd:\temp\tempo.df1', MOVE 'UBCD_sklad_2010_Log' TO 'd:\temp\tempo.df2'
+
+create proc sp_GelLogicalFileNames @path nvarchar(300) as
+begin
+--procedure to get logical file names from backups
+restore filelistonly from disk = @path
+end
+
+go 
+
+alter proc sp_checkBackupS(@bb nvarchar(max)) as
+begin
+	--input is a string with a list of full file names splited by newlines
+	if OBJECT_ID('#res') IS NOT NULL  drop table #res;
+	create table #res(filename nvarchar(400),res int);
+
+	declare @total int ;
+	set @total = 0;
+	declare @curfile nvarchar(200) ;
+	set @curfile = '';
+	--declare cur cursor for select value from string_split(@bb,NCHAR(10));
+	declare cur cursor for select ltrim(splitdata) from master.dbo.fnsplitstring( @bb,nchar(10))
+
+	open cur;
+	while 1=1 begin
+
+		fetch next from cur into @curfile;
+		if @@FETCH_STATUS != 0 break;
+
+		set @curfile = REPLACE(@curfile,nchar(13),'');
+		if @curfile = '' break;
+		print @curfile + ' ==testing' ;
+		declare @gres int;
+		set @gres = -2;
+		if exists(select name from sys.databases where name = 'tempodb') drop database tempodb;
+		exec master.dbo.sp_checkBackup @backupFile = @curfile, @res = @gres OUTPUT
+		print @gres
+		if @gres != 0 BEGIN 
+			set @total = -1;
+			print @curfile + ' ==BAD' ;
+			insert #res(filename, res) values (@curfile, @gres);
+			--break;
+		end
+		else 
+		begin 
+			print @curfile + ' ==OK' ;
+			insert #res(filename, res) values (@curfile,0);
+		end
+		set @curfile = '';
+	
+	end
+	close cur
+	deallocate cur
+	select filename, res from #res;
+end
+
+go
+
+alter proc sp_checkBackup (@backupFile nvarchar(200), @res int OUTPUT )
+
+as
+begin
+--checks-restore current backup
+select @res = -3;
+
+create table #ff (LogicalName nvarchar(100)
+,_1 nvarchar(100)
+,_2 nvarchar(100)
+,_3 nvarchar(100)
+,_4 nvarchar(100)
+,_5 nvarchar(100)
+,_6 nvarchar(100)
+,_7 nvarchar(100)
+,_8 nvarchar(100)
+,_9 nvarchar(100)
+,_10 nvarchar(100)
+,_11 nvarchar(100)
+,_12 nvarchar(100)
+,_13 nvarchar(100)
+,_14 nvarchar(100)
+,_15 nvarchar(100)
+,_16 nvarchar(100)
+,_17 nvarchar(100)
+,_18 nvarchar(100)
+,_19 nvarchar(100)
+--,_20 nvarchar(100)
+--,_21 nvarchar(100)
+);
+
+
+insert #ff (LogicalName
+,_1
+,_2
+,_3
+,_4
+,_5
+,_6
+,_7
+,_8
+,_9
+,_10
+,_11
+,_12
+,_13
+,_14
+,_15
+,_16
+,_17
+,_18
+,_19
+--,_20
+--,_21
+)  exec master.dbo.sp_GelLogicalFileNames @backupFile;
+	
+--select LogicalName from #ff;
+
+
+declare @tempfold nvarchar(200);
+set @tempfold = N'd:\temp\tempo.df';
+declare @tempoDB sysname;
+set @tempoDB = '';
+
+declare	@NumOfFiles int;
+set @numOfFiles = 0;
+select	@NumOfFiles = count(*) from #ff; 
+
+declare @i int;
+set @i = 1;
+declare @movesql nvarchar(max) ;
+set @movesql = '';
+declare @restoresql nvarchar(max) ;
+set @restoresql = '';
+declare @fname nvarchar(200) ;
+set @fname = '';
+
+while @i <= @NumOfFiles begin
+	exec sp_executesql N'select @fnameinner = LogicalName from (select  LogicalName , row_number() over (order by LogicalName ) as Linenumber from #ff) as line where Linenumber = @iinner'
+				,N'@iinner int, @fnameinner nvarchar(200) OUTPUT'
+				, @iinner = @i 
+				, @fnameinner = @fname OUTPUT;
+	set @movesql = @movesql +', MOVE ''' + @fname+''' TO '''+ @tempfold+cast(@i as nvarchar(2))+'''';
+	set @i = @i + 1;
+end
+set @tempoDB = 'tempoDb';
+set @restoresql = 'restore database '+@tempoDB+' from disk = '''+@backupFile+''' with '+SUBSTRING(@movesql,2, len(@movesql));
+
+drop table #ff
+--select @restoresql
+
+
+BEGIN TRY  
+	print @restoresql;
+	exec sp_executesql @restoresql;
+	if @@ERROR = 0 begin
+		set @res = 0;
+	end
+	else
+	begin
+		set @res = -1;
+	end
+    -- RAISERROR with severity 11-19 will cause execution to   
+    -- jump to the CATCH block.  
+    --RAISERROR ('Error raised in TRY block.', -- Message text.  
+    --           16, -- Severity.  
+    --           1 -- State.  
+    --           );  
+END TRY  
+BEGIN CATCH  
+    DECLARE @ErrorMessage NVARCHAR(4000);  
+    DECLARE @ErrorSeverity INT;  
+    DECLARE @ErrorState INT;  
+
+    SELECT   
+        @ErrorMessage = ERROR_MESSAGE(),  
+        @ErrorSeverity = ERROR_SEVERITY(),  
+        @ErrorState = ERROR_STATE();  
+	print @ErrorMessage
+	print @ErrorSeverity
+	print @ErrorState
+    -- Use RAISERROR inside the CATCH block to return error  
+    -- information about the original error that caused  
+    -- execution to jump to the CATCH block.  
+    --RAISERROR (@ErrorMessage, -- Message text.  
+    --           @ErrorSeverity, -- Severity.  
+    --           @ErrorState -- State.  
+    --           );  
+	set @res = -33;
+END CATCH;
+
+end
+go
+
+/*
+checks whole dir
+*/
+create procedure za_check_dir_with_backups  (@dir nvarchar(100)) as
+begin
+if object_id('tempdb..#dirs') is not  null drop table #dirs;
+if object_id('tempdb..#files') is not null drop table #files;
+--GO;
+--declare @dir nvarchar(100) = N'j:\b\';
+
+create table #dirs (subdirectory nvarchar(100), depth int);
+insert into #dirs(subdirectory, depth) execute master.dbo.xp_dirtree @dir,1;
+
+create table #files (filename nvarchar(200), depth int, _3 int);
+insert into #files (filename, depth, _3) execute master.dbo.xp_dirtree @dir,1,1 
+
+delete #files from #dirs as d where #files.filename in (select subdirectory from #dirs);
+
+declare @names nvarchar(max);
+set @names = N'';
+select @names = @names + @dir+filename + char(13)+char(10) from #files where filename like '%-FULL%' 
+--select @names;
+execute master.dbo.sp_checkBackupS @names
+end
+-----
+create FUNCTION [dbo].[fnSplitString] 
+( 
+    @string NVARCHAR(MAX), 
+    @delimiter CHAR(1) 
+) 
+RETURNS @output TABLE(splitdata NVARCHAR(200) 
+) 
+BEGIN 
+    DECLARE @start INT, @end INT 
+    SELECT @start = 1, @end = CHARINDEX(@delimiter, @string) 
+    WHILE @start < LEN(@string) + 1 BEGIN 
+        IF @end = 0  
+            SET @end = LEN(@string) + 1
+
+        INSERT INTO @output (splitdata)  
+        VALUES(SUBSTRING(@string, @start, @end - @start)) 
+        SET @start = @end + 1 
+        SET @end = CHARINDEX(@delimiter, @string, @start)
+
+    END 
+    RETURN 
+
+END
